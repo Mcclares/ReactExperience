2af652b660274ff97c42656bf1a17ca14ad4a973
Mcclares <86326552+Mcclares@users.noreply.github.com>
2022-01-28 18:50:12 +0300

    teba

---
+++ trello-clone/src/Column.tsx
@@ -0,0 +1,17 @@
+import {ColumnContainer,ColumnTitle } from "./styles"
+import {Children, FC} from "react"
+import {AddNewItem } from "./AddNewItem"
+type ColumnProps = {
+    text: string
+}
+export const Column: FC<ColumnProps> = ({text,children}) => {
+    return (
+        <ColumnContainer>
+            <ColumnTitle>{text}</ColumnTitle>
+            {children}
+            <AddNewItem toggleButtonText="+ Add another task" onAdd={console.log}/>
+        </ColumnContainer>
+
+
+    )
+}
